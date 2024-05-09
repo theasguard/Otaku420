3885399ce07d24ca5c38e1b9029214c3e696ae18
theasguard <57173230+theasguard@users.noreply.github.com>
2024-05-08 21:01:28 -0600

    I'm not sure 🤔

--- Nyaa.py
+++ Nyaa.py
@@ -36,13 +36,11 @@ def _get_episode_sources_pack(self, show, anilist_id, episode):
     regex_ep_range = r'\s(?:\d+(?:[-~]\d+)?)(?:-(?:\d+(?:[-~]\d+)?))?'
     regex_season = r'\b(?:s|season|series)\s*(\d+)\b'
     regex_title = r'^r'(.+)'
-    regex_lang = r'\[(?:vostfr|eng|dual|multi)\]'
     rex = re.compile(regex)
     rex_ep = re.compile(regex_ep)
     rex_ep_range = re.compile(regex_ep_range)
     rex_season = re.compile(regex_season)
     rex_title = re.compile(regex_title)
-    rex_lang = re.compile(regex_lang)
 
     url = '%s?f=0&c=1_2&q=%s&s=seeders&&o=desc' % (self._BASE_URL, urllib_parse.quote_plus(query))
     return self._process_nyaa_backup(url, anilist_id, 2, episode.zfill(2), True)
@@ -67,13 +65,11 @@ def _process_nyaa_episodes(self, url, episode, season=None):
     regex_ep_range = r'\s(?:\d+(?:[-~]\d+)?)(?:-(?:\d+(?:[-~]\d+)?))?'
     regex_season = r'\b(?:s|season|series)\s*(\d+)\b'
     regex_title = r'^r'(.+)'
-    regex_lang = r'\[(?:vostfr|eng|dual|multi)\]'
     rex = re.compile(regex)
     rex_ep = re.compile(regex_ep)
     rex_ep_range = re.compile(regex_ep_range)
     rex_season = re.compile(regex_season)
     rex_title = re.compile(regex_title)
-    rex_lang = re.compile(regex_lang)
     filtered_list = []
 
     for idx, torrent in enumerate(list_):
